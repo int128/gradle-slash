173511f45175a1ffaa102e23515488000c84d273
int128 <int128@gmail.com>
2014-12-12 10:29:42 +0900

    Rename plugin

--- src/main/groovy/org/hidetake/gradle/slash/SlashPlugin.groovy
+++ src/main/groovy/org/hidetake/gradle/slash/SlashPlugin.groovy
@@ -1,9 +1,9 @@
-package com.example
+package org.hidetake.gradle.slash
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class HelloPlugin implements Plugin<Project> {
+class SlashPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         // TODO
--- src/test/groovy/org/hidetake/gradle/slash/SlashPluginSpec.groovy
+++ src/test/groovy/org/hidetake/gradle/slash/SlashPluginSpec.groovy
@@ -1,9 +1,9 @@
-package com.example
+package org.hidetake.gradle.slash
 
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
-class HelloPluginSpec extends Specification {
+class SlashPluginSpec extends Specification {
 
     def "applying the plugin"() {
         given:
@@ -11,7 +11,7 @@ class HelloPluginSpec extends Specification {
 
         when:
         project.with {
-            apply plugin: 'com.example.hello'
+            apply plugin: 'org.hidetake.slash'
         }
 
         then:
